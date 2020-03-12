a815839fd84ace2d4143a8ac14eb0f01ae420167
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-03-12 16:35:37 -0400

    axios call Ep

---
+++ src/api/getEpisodes.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const getEpisodes = () => {
+    return axios
+    .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
+    .then(res => res)
+}
